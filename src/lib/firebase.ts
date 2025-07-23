8823bb893ba511bd83fa4ae0036e18289624109c
Ratan Lal Mourya <lalmouryaratan@gmail.com>
2025-07-23 17:17:30 +0000

    File changes

---
+++ src/lib/firebase.ts
@@ -0,0 +1,21 @@
+
+import { initializeApp, getApp, getApps } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+import { getStorage } from 'firebase/storage';
+
+const firebaseConfig = {
+  projectId: "echostream-ia2gt",
+  appId: "1:1053615411237:web:d1e87a95c0c9edd644efcc",
+  storageBucket: "echostream-ia2gt.firebasestorage.app",
+  apiKey: "AIzaSyDs988O5BffPwQCSi6zogTjLADLVpVkkbQ",
+  authDomain: "echostream-ia2gt.firebaseapp.com",
+  measurementId: "",
+  messagingSenderId: "1053615411237",
+};
+
+// Initialize Firebase
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+const db = getFirestore(app);
+const storage = getStorage(app);
+
+export { app, db, storage };
