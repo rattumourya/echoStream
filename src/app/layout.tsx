07493fd6837483ae137b9478e49096ebc4ae2018
Ratan Lal Mourya <lalmouryaratan@gmail.com>
2025-07-27 12:08:04 +0000

    App Icon on the crome tab
    Change favicon.ico from firebase icon to Echo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,6 +20,7 @@ export default function RootLayout({
   return (
     <html lang="en" className="dark">
       <head>
+        <link rel="icon" href="/logo.svg" type="image/svg+xml" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700&display=swap" rel="stylesheet" />
